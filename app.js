c91dc3aa7862c3eb2f8a0f0977341623a651bdc9
caesar <newman0934@gmail.com>
2019-10-30 18:37:26 +0800

    confirm user passowrd and unique email

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require('express')
-const handlebars = require("express-handlebars")
 const db = require("./models")
 const app = express()
 const port = 3000
-const bodyParser = require("body-parser")
+
+const handlebars = require("express-handlebars")
 app.engine("handlebars",handlebars({ defaultLayout: 'main' }))
 app.set("view engine","handlebars")
 
+const bodyParser = require("body-parser")
 app.use(bodyParser.urlencoded({extended:true}))
+
+const session =require("express-session")
+app.use(session({secret:"secret", resave:"false",saveUninitialized:"false"}))
+
+const flash = require("connect-flash")
+app.use(flash())
+app.use((req,res,next) => {
+    res.locals.success_messages = req.flash("success_messages")
+    res.locals.error_messages = req.flash("error_messages")
+    next()
+})
+
+
+
 
 
 app.listen(port, () => {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,21 +1,38 @@
-const bcrypt = require('bcrypt-nodejs') 
-const db = require('../models')
-const User = db.User
+const bcrypt = require("bcrypt-nodejs");
+const db = require("../models");
+const User = db.User;
 
 const userController = {
   signUpPage: (req, res) => {
-    return res.render('signup')
+    return res.render("signup");
   },
 
   signUp: (req, res) => {
-    User.create({
-      name: req.body.name,
-      email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10), null)
-    }).then(user => {
-      return res.redirect('/signin')
-    })
+    if (req.body.passwordCheck !== req.body.password) {
+      req.flash('error_messages', '兩次密碼輸入不同！')
+      return res.redirect('/signup')
+    } else {
+      User.findOne({ where: { email: req.body.email } }).then(user => {
+        if (user) {
+          req.flash("error_messages", "信箱重複!");
+          return res.redirect("/signup");
+        } else {
+          User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: bcrypt.hashSync(
+              req.body.password,
+              bcrypt.genSaltSync(10),
+              null
+            )
+          }).then(user => {
+            req.flash("seccess_messages", "註冊成功!");
+            return res.redirect("/signin");
+          });
+        }
+      });
+    }
   }
-}
+};
 
-module.exports = userController
+module.exports = userController;
