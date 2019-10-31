60626e5f2617cd836e72104bb61f02e0a14988ed
caesar <newman0934@gmail.com>
2019-10-31 18:15:23 +0800

    add authentication before rendering pages

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,40 @@
-const restControllers = require("../controllers/restControllers")
-const adminController = require("../controllers/adminController")
-const userController = require("../controllers/userController")
+const restControllers = require("../controllers/restControllers");
+const adminController = require("../controllers/adminController");
+const userController = require("../controllers/userController");
 
 module.exports = (app, passport) => {
-    app.get("/",(req,res) => {
-        res.redirect("/restaurants")
-    })
-    app.get("/restaurants", restControllers.getRestaurants)
+    const authenticated = (req, res, next) => {
+        if (req.isAuthenticated()) {
+            return next()
+        }
+        res.redirect('/signin')
+    }
+    const authenticatedAdmin = (req, res, next) => {
+        if (req.isAuthenticated()) {
+          if (req.user.isAdmin) { return next() }
+          return res.redirect('/')
+        }
+        res.redirect('/signin')
+      }
 
-    app.get("/admin", (req,res) => {
-        res.redirect("/admin/restaurants")
-    })
-    app.get("/admin/restaurants",adminController.getRestaurants)
+    app.get('/', authenticated, (req, res) => res.redirect('/restaurants'))
+    app.get('/restaurants', authenticated, restControllers.getRestaurants)
 
-    app.get('/signup', userController.signUpPage)
-    app.post('/signup', userController.signUp)
+    app.get('/admin', authenticatedAdmin, (req, res) => res.redirect('/admin/restaurants'))
+    app.get('/admin/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
-    app.get('/signin', userController.signInPage)
-    app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
-    
-    app.get('/logout', userController.logout)
-}
+    app.get("/signup", userController.signUpPage);
+    app.post("/signup", userController.signUp);
+
+    app.get("/signin", userController.signInPage);
+    app.post(
+        "/signin",
+        passport.authenticate("local", {
+            failureRedirect: "/signin",
+            failureFlash: true
+        }),
+        userController.signIn
+    );
+
+    app.get("/logout", userController.logout);
+};
