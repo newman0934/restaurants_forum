30a19b8070323c89037ba25d814237a302e0616d
java5575 <newman0934@gmail.com>
2019-11-03 16:06:51 +0800

    add process.env.PORT

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const db = require("./models")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 const methodOverride = require("method-override")
 app.use(methodOverride("_method"))
