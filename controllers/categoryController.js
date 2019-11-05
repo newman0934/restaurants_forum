c68173ed7176ea40839cf18d6750ff360e2864c7
caesar <newman0934@gmail.com>
2019-11-05 18:39:13 +0800

    Add admin categories delete feature

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -39,6 +39,13 @@ let categoryController = {
                 })
             })
         }
+    },
+    deleteCategory: (req,res) => {
+        return Category.findByPk(req.params.id).then( category => {
+            category.destroy().then(category => {
+                res.redirect("/admin/categories")
+            })
+        })
     }
 }
 
